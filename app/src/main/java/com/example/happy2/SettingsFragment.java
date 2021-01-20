f805e4c62456f582ae77fa1925afc0ba4e3d0a0d
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-20 05:19:51 +0100

    settings

--- app/src/main/java/com/example/happy2/SettingsFragment.java
+++ app/src/main/java/com/example/happy2/SettingsFragment.java
@@ -1,13 +1,39 @@
 package com.example.happy2;
 
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.widget.TimePicker;
 
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements TimePickerDialog.OnTimeSetListener {
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        final Preference preferenceTimerOn = getPreferenceManager().findPreference("switchPrefSelectedTime");
+        preferenceTimerOn.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                ((SwitchPreference) preference).setChecked((boolean) newValue);
+                return true;
+            }
+        });
+
+        final Preference preferenceAbout = getPreferenceManager().findPreference("editTextbout");
+        preferenceAbout.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(Preference preference, Object newValue) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
     }
 }
