c7dc11a08789305ede1fe8889d35c7c3abc099c9
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-22 20:16:34 +0100

    Entity Idea

---
+++ app/src/main/java/com/example/happy2/Idea.java
@@ -0,0 +1,35 @@
+package com.example.happy2;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "idea_table")
+public class Idea {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String what;
+    private String adInfo;
+
+    public Idea(String what, String adInfo) {
+        this.what = what;
+        this.adInfo = adInfo;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getWhat() {
+        return what;
+    }
+
+    public String getAdInfo() {
+        return adInfo;
+    }
+}
