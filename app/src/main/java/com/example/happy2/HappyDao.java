d25801e7dc01f8a348fd470ba3f283a1c1cfc1b2
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-22 22:14:27 +0100

    HappyDao (very beta!!!!)

---
+++ app/src/main/java/com/example/happy2/HappyDao.java
@@ -0,0 +1,29 @@
+package com.example.happy2;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface HappyDao {
+
+    @Insert
+    void insert(HappyThing happyThing);
+
+    @Update
+    void update(HappyThing happyThing);
+
+    @Delete
+    void delete(HappyThing happyThing);
+
+    @Query("DELETE FROM happy_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM happy_table ORDER BY what DESC")
+    LiveData<List<HappyThing>> getAll();
+}
