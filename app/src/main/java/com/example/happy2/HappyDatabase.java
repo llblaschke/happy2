d6836d04160aaf311c704822dc3f1868030b4b8f
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-23 01:18:58 +0100

    HappyDatabase populate empty db

--- app/src/main/java/com/example/happy2/HappyDatabase.java
+++ app/src/main/java/com/example/happy2/HappyDatabase.java
@@ -1,19 +1,27 @@
 package com.example.happy2;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import static com.example.happy2.R.string.empty_list;
 
 @Database(entities = {HappyThing.class}, version = 1)
 public abstract class HappyDatabase extends RoomDatabase {
 
     private static HappyDatabase instance;
     public abstract HappyDao happyDao();
+    private static String emptyList = Resources.getSystem().getString(empty_list);
 
-    public static synchronized HappyDatabase getInstance(Context context){
-        if(instance == null){
+
+    public static synchronized HappyDatabase getInstance(Context context) {
+        if (instance == null) {
             instance = Room.databaseBuilder(
                     context.getApplicationContext(),
                     HappyDatabase.class,
@@ -22,5 +30,28 @@ public abstract class HappyDatabase extends RoomDatabase {
                     .build();
         }
         return instance;
+    }
+
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private HappyDao happyDao;
+
+        private PopulateDbAsyncTask(HappyDatabase db) {
+            happyDao = db.happyDao();
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            happyDao.insert(new HappyThing(emptyList, "", "", "", null));
+            return null;
+        }
     }
 }
