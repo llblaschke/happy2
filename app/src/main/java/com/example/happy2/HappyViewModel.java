c1ec43cc0a422510a11567110ec547f746003994
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-23 01:54:52 +0100

    HappyViewModel

---
+++ app/src/main/java/com/example/happy2/HappyViewModel.java
@@ -0,0 +1,37 @@
+package com.example.happy2;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class HappyViewModel extends AndroidViewModel {
+
+    private HappyRepository repository;
+    private LiveData<List<HappyThing>> allHappyThings;
+
+    public HappyViewModel(@NonNull Application application) {
+        super(application);
+        repository = new HappyRepository(application);
+        allHappyThings = repository.getAllHappyThings();
+    }
+
+    public void insert(HappyThing happyThing){
+        repository.insert(happyThing);
+    }
+
+    public void update(HappyThing happyThing){
+        repository.update(happyThing);
+    }
+
+    public void delete(HappyThing happyThing){
+        repository.delete(happyThing);
+    }
+
+    public void deleteAllHappyThings(){
+        repository.deleteAllHappyThings();
+    }
+}
