e974391a16369f52659617ce69c65f98c5504faf
Lana L. Blaschke <lblaschke@posteo.de>
2021-01-31 03:03:51 +0100

    AddHappyThingFragment: put textview-texts in update mode and CLEAN UP

--- app/src/main/java/com/example/happy2/Fragments/AddHappyThingFragment.java
+++ app/src/main/java/com/example/happy2/Fragments/AddHappyThingFragment.java
@@ -1,6 +1,7 @@
 package com.example.happy2.Fragments;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
@@ -31,12 +32,19 @@ import java.util.Calendar;
 import java.util.HashSet;
 import java.util.List;
 
+import static com.example.happy2.AddActivity.KEY_ADINFO;
+import static com.example.happy2.AddActivity.KEY_EDIT_IDEA_HAPPY;
+import static com.example.happy2.AddActivity.KEY_WHAT;
+import static com.example.happy2.AddActivity.KEY_WHEN;
+import static com.example.happy2.AddActivity.KEY_WHERE;
+import static com.example.happy2.AddActivity.KEY_WITH;
+
 
 public class AddHappyThingFragment extends Fragment {
 
 
     private static final String TEXTVIEW_UPDATED = "updateAddMore";
-    private boolean updateAddMore;
+    private boolean updateAddMore, update;
 
     private TextView textViewAddHappyThing;
     private AutoCompleteTextView acTextViewWhat;
@@ -58,6 +66,7 @@ public class AddHappyThingFragment extends Fragment {
     public static final String TMP_ADINFO = "tmpAdInfo";
 
     private SharedPreferences prefs;
+    private Intent intent;
 
     private HappyViewModel happyViewModel;
     private ArrayAdapter happyWhatAdapter;
@@ -83,7 +92,7 @@ public class AddHappyThingFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            updateAddMore = getArguments().getBoolean((TEXTVIEW_UPDATED));
+            updateAddMore = getArguments().getBoolean((TEXTVIEW_UPDATED), false);
         }else{
             updateAddMore = false;
         }
@@ -96,7 +105,41 @@ public class AddHappyThingFragment extends Fragment {
                              Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_add_happy, container, false);
+        intent = getActivity().getIntent();
 
+        getAllViews(v);
+        prepareButtonSaveAndTextChangedListeners();
+        getUpdateMode();
+        fillTextViews();
+        autoCompleteACTextViews();
+        getCalendar();
+        if (updateAddMore) updateTxtAddHappyThing();
+
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (!update) {
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString(TMP_WHAT, tmpWhat)
+                    .putString(TMP_WITH, tmpWith)
+                    .putString(TMP_WHERE, tmpWhere)
+                    .putString(TMP_ADINFO, tmpAdInfo);
+            editor.commit();
+        }
+    }
+
+
+
+
+    /* ********************************************
+    GET ALL VIEW-ELEMENTS AND IF IN UPDATE MODE
+    *********************************************** */
+
+    // get all needed TextViews and Buttons
+    private void getAllViews(View v) {
         textViewAddHappyThing = v.findViewById(R.id.txtAddHappyThing);
         acTextViewWhat = v.findViewById(R.id.acTextViewWhat);
         acTextViewWith = v.findViewById(R.id.acTextViewWith);
@@ -105,11 +148,65 @@ public class AddHappyThingFragment extends Fragment {
         acTextViewAdInfo = v.findViewById(R.id.acTextViewAdInfo);
         buttonChangeDate = v.findViewById(R.id.btnChangeDate);
         buttonSave = v.findViewById(R.id.btnSave);
+    }
 
-        if(updateAddMore) {
-            updateTxtAddHappyThing();
+
+    // check if we are in update mode
+    private void getUpdateMode() {
+        if (intent.hasExtra(KEY_EDIT_IDEA_HAPPY)) {
+            // if we are in edit mode
+            update = intent.getBooleanExtra(KEY_EDIT_IDEA_HAPPY, false);
+        } else {
+            update = false;
         }
+    }
 
+
+
+    /* ********************************************
+    FILL TEXT VIEWS WITH CORRECT TEXT
+    *********************************************** */
+
+    // fill textViews with either text to update or unsaved text
+    private void fillTextViews() {
+        String what = null;
+        String where = null;
+        String with = null;
+        String adInfo = null;
+        String when = getString(R.string.text_today);
+        if (update){
+            // if we are in edit mode
+            what = intent.getStringExtra(KEY_WHAT);
+            with = intent.getStringExtra(KEY_WITH);
+            where = intent.getStringExtra(KEY_WHERE);
+            adInfo = intent.getStringExtra(KEY_ADINFO);
+            when = intent.getStringExtra(KEY_WHEN);
+        } else {
+            // if there was unsaved data
+            if (prefs.contains(TMP_WHAT)) what = prefs.getString(TMP_WHAT, "");
+            if (prefs.contains(TMP_WITH)) with = prefs.getString(TMP_WITH, "");
+            if (prefs.contains(TMP_WHERE)) where = prefs.getString(TMP_WHERE, "");
+            if (prefs.contains(TMP_ADINFO)) adInfo = prefs.getString(TMP_ADINFO, "");
+        }
+        acTextViewWhat.setText(what);
+        acTextViewWith.setText(with);
+        acTextViewWhere.setText(where);
+        acTextViewAdInfo.setText(adInfo);
+        editTextWhen.setText(when);
+    }
+
+    // if add-more mode, set new text as title
+    public void updateTxtAddHappyThing() {
+        textViewAddHappyThing.setText(R.string.text_what_made_you_happy_what_else);
+    }
+
+
+
+    /* ********************************************
+    SAVE BUTTON AND TEXTCHANGEDLISTENERS
+    *********************************************** */
+
+    private void prepareButtonSaveAndTextChangedListeners() {
         buttonChangeDate.setOnClickListener(btnChangeDate);
 
         // Save button enables only if all EditTexts contain input
@@ -119,47 +216,7 @@ public class AddHappyThingFragment extends Fragment {
         acTextViewWith.addTextChangedListener(addHappyTextWatcher);
         acTextViewWhere.addTextChangedListener(addHappyTextWatcher);
         acTextViewAdInfo.addTextChangedListener(addHappyTextWatcher);
-        editTextWhen.addTextChangedListener(addHappyTextWatcher);
-
-        calendar = Calendar.getInstance();
-        year = calendar.get(Calendar.YEAR);
-        month = calendar.get(Calendar.MONTH);
-        day = calendar.get(Calendar.DAY_OF_MONTH);
-
-        if (prefs.contains(TMP_WHAT)){
-            tmpWhat = prefs.getString(TMP_WHAT, "");
-            acTextViewWhat.setText(tmpWhat);
-        }
-        if (prefs.contains(TMP_WITH)){
-            tmpWith = prefs.getString(TMP_WITH, "");
-            acTextViewWith.setText(tmpWith);
-        }
-        if (prefs.contains(TMP_WHERE)){
-            tmpWhere = prefs.getString(TMP_WHERE, "");
-            acTextViewWhere.setText(tmpWhere);
-        }
-        if (prefs.contains(TMP_ADINFO)){
-            tmpAdInfo = prefs.getString(TMP_ADINFO, "");
-            acTextViewAdInfo.setText(tmpAdInfo);
-        }
-
-        autoCompleteACTextViews();
-        return v;
     }
-
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString(TMP_WHAT, tmpWhat)
-                .putString(TMP_WITH, tmpWith)
-                .putString(TMP_WHERE, tmpWhere)
-                .putString(TMP_ADINFO, tmpAdInfo);
-        editor.commit();
-    }
-
-
 
     private View.OnClickListener btnClickSave = new View.OnClickListener() {
         @Override
@@ -200,6 +257,20 @@ public class AddHappyThingFragment extends Fragment {
 
 
 
+
+
+    /* ********************************************
+    CALENDAR AND CHANGE DATE BUTTON
+    *********************************************** */
+
+    private void getCalendar() {
+        calendar = Calendar.getInstance();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+
     private View.OnClickListener btnChangeDate = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
@@ -229,9 +300,11 @@ public class AddHappyThingFragment extends Fragment {
         editTextWhen.setText(string);
     }
 
-    public void updateTxtAddHappyThing() {
-        textViewAddHappyThing.setText(R.string.text_what_made_you_happy_what_else);
-    }
+
+
+    /* ********************************************
+    AUTOCOMPLETE TEXTVIEWS
+    *********************************************** */
 
     // set all adapters and thresholds for the autocomplete textviews
     private void autoCompleteACTextViews() {
